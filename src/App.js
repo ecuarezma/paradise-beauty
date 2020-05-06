a4cdaf14183aae61c3f19267fb126ba251e428b2
Edgar Cuarezma <ecuarezma@gmail.com>
2020-05-06 13:04:05 -0500

    Add images from unsplash to assets folder

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <p>sup react page!</p>;
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <h1>Hello from React App!</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
