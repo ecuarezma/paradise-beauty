33644da5453bda443c022a29eb2677eed528d1ed
Edgar Cuarezma <ecuarezma@gmail.com>
2020-05-08 18:09:36 -0500

    Add Home component and place background image

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React from "react";
+import Home from "./components/pages/Home/";
 import "./App.css";
 
-function App() {
-  fetch("/user")
-    .then((res) => res.json())
-    .then((data) => console.log(data));
-  return (
-    <div className="container">
-      <div className="jumbotron">
-        <header className="title">
-          <h1>Hello from React App!</h1>
-        </header>
-      </div>
-    </div>
-  );
-}
+const App = () => {
+  return <Home />;
+};
 
 export default App;
---
+++ src/components/pages/Home/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classes from "./Home.module.scss";
+
+import salonChairs from "../../../assets/unsplash/salonchairs.jpg";
+
+const Home = () => {
+  return (
+    <div className={classes.Home}>
+      {/* <div className="container">
+        <div className="jumbotron">
+          <h1>The Homepage!</h1>
+        </div>
+      </div> */}
+    </div>
+  );
+};
+
+export default Home;
