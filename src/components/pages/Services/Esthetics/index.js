352904f9d82c8b75c946e8f767cf179b54497734
Edgar Cuarezma <ecuarezma@gmail.com>
2020-07-01 19:13:20 -0500

    Add images to esthetics component

--- src/components/pages/Services/Esthetics/index.js
+++ src/components/pages/Services/Esthetics/index.js
@@ -1,5 +1,14 @@
 import React from "react";
+import classes from "./Esthetics.module.css";
+
+import mask from "../../../../assets/unsplash/mask.jpg";
+import nails from "../../../../assets/unsplash/nails.jpg";
 
 export default function Esthetics() {
-  return <div>"esthetics component"</div>;
+  return (
+    <div className={classes.Esthetics}>
+      <div className={classes.nails} />
+      <div className={classes.mask} />
+    </div>
+  );
 }
--- src/components/pages/Services/Treatment/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Treatment() {
-  return <div>"treatment component"</div>;
-}
