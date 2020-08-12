d880ce861cfbf5459af06a611fe46706538fc0ae
Edgar Cuarezma <ecuarezma@gmail.com>
2020-08-12 12:45:28 -0500

    Add products section

--- src/components/pages/Products/index.js
+++ src/components/pages/Products/index.js
@@ -1,12 +1,31 @@
 import React from "react";
+import classes from "../Services/Services.module.scss";
 
 export default function Products() {
   return (
     <article>
-      <header>
-        <h2>Products</h2>
-      </header>
-      <hr />
+      <header className={classes.bottleBg} />
+      <div className={classes.info}>
+        <h3>PRODUCTS</h3>
+        <ul className={classes.products}>
+          <li>Lanza</li>
+          <li>Salon-In</li>
+          <li>Salerm</li>
+          <li>Cosmetics</li>
+          <li>Hair Food</li>
+          <li>Maui</li>
+          <li>OGX</li>
+          <li>Neutogena</li>
+          <li>Ponds</li>
+          <li>Paul</li>
+          <li>Mitchel</li>
+          <li>Felps</li>
+          <li>Professional</li>
+          <li>Loreal</li>
+          <li>Hempz</li>
+          <li>OPI</li>
+        </ul>
+      </div>
     </article>
   );
 }
